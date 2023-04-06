a0d17b3a88378728a5cae0ce6b103f01d612917b
Tiago Fialho <tiago.chem@gmail.com>
2023-04-06 07:09:28 -0300

    feat: insert vw_consumed_in_2023

--- mywinesFuncs.sql
+++ mywinesFuncs.sql
@@ -89,3 +89,55 @@ AS
         LEFT JOIN storage_condition ON wine_bottle.storage_condition_id = storage_condition.id
         LEFT JOIN profile ON wine_bottle.profile_id = profile.id
 GO
+
+--vinhos consumidos em 2023
+CREATE OR ALTER VIEW vw_consumed_in_2023
+AS
+    SELECT
+        wine_bottle.consumption_date AS 'Consumido em',
+        wine_bottle.bottle_name AS 'Vinho',
+        variety.variety_name AS 'Uva',
+        winery.winery_name AS 'Produtor',
+        wine_bottle.score AS 'Nota',
+        style.style_name AS 'Estilo'
+        -- country.country_name AS 'Pais',
+        -- region.region_name AS 'Regiao',
+        -- subregion.subregion_name AS 'Subregiao',
+        -- storage_condition.storage_name AS 'Armazenamento'
+    FROM wine_bottle
+        LEFT JOIN variety ON wine_bottle.variety_id = variety.id
+        LEFT JOIN winery ON wine_bottle.winery_id = winery.id
+        LEFT JOIN style ON wine_bottle.style_id = style.id
+        LEFT JOIN storage_condition ON wine_bottle.storage_condition_id = storage_condition.id
+    WHERE
+        (wine_bottle.consumption_date BETWEEN '2023-01-01' AND '2023-12-31')
+        AND storage_condition_id = 2
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM vw_locations
+SELECT * FROM vw_bottles
+
+SELECT * FROM winery
+SELECT * FROM country WHERE country_name LIKE '%italy%'
+SELECT * FROM region WHERE region_name LIKE '%tu%'
+SELECT * FROM subregion WHERE subregion_name LIKE '%mon%'
+SELECT * FROM style
+SELECT * FROM storage_condition
+SELECT * FROM variety WHERE variety_name LIKE '%san%'
+
+
+SELECT * FROM storage_condition
+SELECT * FROM [location]
+
+SELECT * FROM variety WHERE variety_name LIKE '%sy%'
